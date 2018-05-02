2fe3e0d7cf27391a6a3565263f45443bd301ca99
jiangjian <jiangjian199204@gmail.com>
2018-05-02 14:34:06 +0800

    add netty echo server and client demo

---
+++ src/main/java/com/jiangjian/learning/netty/chap2/EchoServer.java
@@ -0,0 +1,45 @@
+package com.jiangjian.learning.netty.chap2;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+public class EchoServer {
+    private final int port;
+
+    public EchoServer(int port) {
+        this.port = port;
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        new EchoServer(9090).start();
+    }
+
+    private void start() throws InterruptedException {
+        EventLoopGroup group = null;
+        try {
+            final EchoServerHandler serverHandler = new EchoServerHandler();
+            group = new NioEventLoopGroup();
+            ServerBootstrap b = new ServerBootstrap();
+            b.group(group)
+                    .channel(NioServerSocketChannel.class)
+                    .localAddress(port)
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        public void initChannel(SocketChannel ch) throws Exception {
+                            ch.pipeline().addLast(serverHandler);
+                        }
+                    });
+            ChannelFuture f = b.bind().sync();
+            f.channel().closeFuture().sync();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }finally {
+            group.shutdownGracefully().sync();
+        }
+    }
+}
