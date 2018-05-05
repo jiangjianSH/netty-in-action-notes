2d93ab8330919196231a7ea6944ea23a6b03de7e
jiangjian <jiangjian199204@gmail.com>
2018-05-05 21:42:32 +0800

    add netty provided codes and handler

---
+++ src/main/java/com/jiangjian/learning/netty/chap10/HttpCompressionInitializer.java
@@ -0,0 +1,28 @@
+package com.jiangjian.learning.netty.chap10;
+
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.handler.codec.http.HttpClientCodec;
+import io.netty.handler.codec.http.HttpContentDecompressor;
+import io.netty.handler.codec.http.HttpServerCodec;
+
+public class HttpCompressionInitializer extends ChannelInitializer<Channel> {
+    private final boolean isClient;
+
+    public HttpCompressionInitializer(boolean isClient) {
+        this.isClient = isClient;
+    }
+
+    @Override
+    protected void initChannel(Channel ch) throws Exception {
+        ChannelPipeline pipeline = ch.pipeline();
+        if(isClient) {
+            pipeline.addLast("codec", new HttpClientCodec());
+            pipeline.addLast("decompressor", new HttpContentDecompressor());
+        } else {
+            pipeline.addLast("codec", new HttpServerCodec());
+            pipeline.addLast("compressor", new HttpContentDecompressor());
+        }
+    }
+}
